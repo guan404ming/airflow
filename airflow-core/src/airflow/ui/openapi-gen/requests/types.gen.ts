cc7ae556fe35f44f7ccb42dc02fdc2a021a27ef5
Guan-Ming (Wesley) Chiu <105915352+guan404ming@users.noreply.github.com>
2026-02-04 17:13:32 +0800

    Add task_display_name to LightGridTaskInstanceSummary model
    Signed-off-by: Guan-Ming (Wesley) Chiu <105915352+guan404ming@users.noreply.github.com>

--- airflow-core/src/airflow/ui/openapi-gen/requests/types.gen.ts
+++ airflow-core/src/airflow/ui/openapi-gen/requests/types.gen.ts
@@ -1995,6 +1995,7 @@ export type HistoricalMetricDataResponse = {
  */
 export type LightGridTaskInstanceSummary = {
     task_id: string;
+    task_display_name: string;
     state: TaskInstanceState | null;
     child_states: {
     [key: string]: (number);
